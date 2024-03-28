1d8d390c1dde50cefbdbe2ce5ee3a1b1f65b0d9e
Trevor West <trevorwest@duck.com>
2024-03-27 20:44:24 -0500

    Add pinkDiv, hOne, paraTwo

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const h3 = document.createElement("h3");
 h3.textContent = "I'm a blue h3!";
 h3.style.cssText = "color: blue";
 container.appendChild(h3);
+
+const pinkDiv = document.createElement("div");
+pinkDiv.style.cssText = "background-color: pink; border: 3px solid black";
+container.appendChild(pinkDiv);
+
+const hOne = document.createElement("h1");
+hOne.textContent = "I'm in a div";
+const paraTwo = document.createElement("p");
+paraTwo.textContent = "ME TOO!";
+pinkDiv.appendChild(hOne);
+pinkDiv.appendChild(paraTwo);
