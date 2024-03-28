db85388a95d588349f4941daf45a0291b0fa6cd7
Trevor West <trevorwest@duck.com>
2024-03-27 20:36:27 -0500

    Add blue text h3 using js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const paragraph = document.createElement("p");
 paragraph.textContent = "Hey I'm red!";
 paragraph.style.cssText = "color: red";
 container.appendChild(paragraph);
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a blue h3!";
+h3.style.cssText = "color: blue";
+container.appendChild(h3);
